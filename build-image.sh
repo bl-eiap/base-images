157966445a39b2ddb4b21f503ab402fe93eb0b3f
Phu Thai Ho <phu.ho@nokia-bell-labs.com>
2020-09-08 16:14:44 +0200

    feat: add docker-compose image

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -f semantic-release/Dockerfile -t semantic-release semantic-release
+SEMANTIC_RELEASE_DIR=semantic-release
+docker build -f ${SEMANTIC_RELEASE_DIR}/Dockerfile -t ${SEMANTIC_RELEASE_DIR} ${SEMANTIC_RELEASE_DIR}
+
+DOCKER_COMPOSE_DIR=docker-compose
+docker build -f ${DOCKER_COMPOSE_DIR}/Dockerfile -t ${DOCKER_COMPOSE_DIR} ${DOCKER_COMPOSE_DIR}
