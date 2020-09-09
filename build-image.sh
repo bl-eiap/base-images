545f188871eb242dd038b08d21a5b6c11e5e15ad
Phu Thai Ho <phu.ho@nokia-bell-labs.com>
2020-09-09 14:59:56 +0200

    fix: do not use cache to build images

--- build-image.sh
+++ build-image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SEMANTIC_RELEASE_DIR=semantic-release
-docker build -f ${SEMANTIC_RELEASE_DIR}/Dockerfile -t ${SEMANTIC_RELEASE_DIR} ${SEMANTIC_RELEASE_DIR}
+docker build -f ${SEMANTIC_RELEASE_DIR}/Dockerfile -t ${SEMANTIC_RELEASE_DIR} ${SEMANTIC_RELEASE_DIR} --no-cache
 docker tag ${SEMANTIC_RELEASE_DIR} eiap/${SEMANTIC_RELEASE_DIR}:1.0.0
 docker push eiap/${SEMANTIC_RELEASE_DIR}:1.0.0
 
 DOCKER_COMPOSE_DIR=docker-compose
-docker build -f ${DOCKER_COMPOSE_DIR}/Dockerfile -t ${DOCKER_COMPOSE_DIR} ${DOCKER_COMPOSE_DIR}
+docker build -f ${DOCKER_COMPOSE_DIR}/Dockerfile -t ${DOCKER_COMPOSE_DIR} ${DOCKER_COMPOSE_DIR} --no-cache
 docker tag ${DOCKER_COMPOSE_DIR} eiap/${DOCKER_COMPOSE_DIR}:1.0.0
 docker push eiap/${DOCKER_COMPOSE_DIR}:1.0.0
